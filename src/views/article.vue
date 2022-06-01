3f76cec1a8e32f328047e854c1be8e84acc978df
0709FanLi <385717045@qq.com>
2022-06-01 14:59:59 +0800

    删除defineProps
    defineProps

--- src/views/article.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div v-html="typeCheckChange"></div>
-</template>
-
-<script lang="ts" setup>
-import typeCheckChange from '@/views/md/typeCheckChange.md?raw'
-console.log('typeCheckChange', typeCheckChange)
-
-</script>
